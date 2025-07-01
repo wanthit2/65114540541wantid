891c7317d6cfa7957187bdc95dfef9a8004eeeab
wanthit2 <wantid.ju.65@ubu.ac.th>
2025-07-02 06:13:17 +0700

    ggg

---
+++ mysite/myapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from myapp.views import data, home, info, pdf
+
+# route
+urlpatterns = [
+    path('', home),
+    path('info', info),
+    path('data', data),
+    path('pdf', pdf),
+]
+# winget install httpie
